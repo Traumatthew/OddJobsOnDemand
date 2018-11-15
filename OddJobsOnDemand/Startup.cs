eef98d0e92083a39e98e7272efb6b8f833045659
Traumatthew <43039716+Traumatthew@users.noreply.github.com>
2018-11-15 10:12:36 -0600

    Initial Commit, Added Roles

---
+++ OddJobsOnDemand/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using OddJobsOnDemand.Models;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OddJobsOnDemand.Startup))]
+namespace OddJobsOnDemand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRoles();
+        }
+
+        public void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Contractor"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Contractor";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
